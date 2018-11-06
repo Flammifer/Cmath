cd2472a75d7557134b208e038531a57534048b47
Flammifer <37635688+Flammifer@users.noreply.github.com>
2018-11-06 22:52:11 +0300

    Update 93.py

--- 9.3/93.py
+++ 9.3/93.py
@@ -1,7 +1,7 @@
 from math import log
 import numpy as np
 step = 100000000
-prt_sum = []
+prt_sum = np.array([])
 flag = 1
 n = 0
 
@@ -10,11 +10,12 @@ while flag:
     tmp = 0
     for i in range(n*step, (n-1)*step, -1):
         tmp += 1/i
-    prt_sum.append(tmp)
+    prt_sum = np.append(prt_sum, tmp)
     en = 0
     for j in range(n-1, -1, -1):
         en += prt_sum[j]
     en -= log(n*step)
     print("Current approximation:", (en - 0.57721566490153286)/(0.577215664901532))
-    if (en - 0.57721566490153286)/(0.577215664901532) < 10^10:
+    if (en - 0.57721566490153286)/(0.577215664901532) < 10**(-10):
         flag = 0
+print(en)
