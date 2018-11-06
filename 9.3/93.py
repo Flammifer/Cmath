61e36e91476664e8ab5fa4eaac63846b3707f779
Flammifer <37635688+Flammifer@users.noreply.github.com>
2018-11-07 01:13:06 +0300

    Прямая сумма в лоб

--- 9.3/93.py
+++ 9.3/93.py
@@ -5,17 +5,16 @@ prt_sum = np.array([])
 flag = 1
 n = 0
 
+en = 0
+C = 0
 while flag:
     n += 1
     tmp = 0
-    for i in range(n*step, (n-1)*step, -1):
-        tmp += 1/i
-    prt_sum = np.append(prt_sum, tmp)
-    en = 0
-    for j in range(n-1, -1, -1):
-        en += prt_sum[j]
-    en -= log(n*step)
-    print("Current approximation:", (en - 0.57721566490153286)/(0.577215664901532))
-    if (en - 0.57721566490153286)/(0.577215664901532) < 10**(-10):
-        flag = 0
+    C += 1/n
+    if (n % step == 0):
+        en = C - log(n)
+        print("Current approximation:", (en - 0.57721566490153286)/(0.577215664901532))
+        if (abs((en - 0.57721566490153286)/(0.577215664901532)) < 10**(-10)):
+            flag = 0
+
 print(en)
