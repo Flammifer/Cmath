4ff0b2eafd45ebb42bf27398e4bb1f127aeaf5b6
Flammifer <37635688+Flammifer@users.noreply.github.com>
2018-12-03 15:47:39 +0300

    Update 7-1b.py

--- V/7-1b.py
+++ V/7-1b.py
@@ -4,30 +4,31 @@ def func(x):
     return x**3/3+x**2
 
 def dihotom(F, a, b, eps = 10**(-8)):
+    a0 = a
+    b0 = b
+    c = (a+b)/2
+    k = 0
     while 1:
-        d = (b-a)/50
-        c = (b+a)/2
-        u1, u2 = c-d, c+d
-
-        if (F(u1)<F(u2)):
-            b = u2
-        else: a = u1
-
-        if (b-a)<eps:
-            return (b+a)/2
-
-
-
-
-
+        yk = (a0+c)/2
+        if (F(yk) <= F(c)):
+            b0 = c
+            c = yk
+        else:
+            b0 = c
+            c =yk
+        k += 1
+        if (b0-a0) < 10**(-5):
+            print("Steps: ", k)
+            return (b0+a0)/2
 
 def check_extr(F, x, step = 10**(-5)):
     d2 = (F(x+step)-F(x-step))/(2*step)
     if (abs(d2) < 10**(-5)):
         return 1
     else: return 0
+
 print(dihotom(func, -1, 1))
-print(dihotom(func, -3, -1))
+print(dihotom(func, -33, -1))
 
 print(check_extr(func, 0.0))
 print(check_extr(func, -5.0))
