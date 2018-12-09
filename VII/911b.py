f9a05677897d2fb3a3114ac9a036ee2ebc1b3cbf
Flammifer <37635688+Flammifer@users.noreply.github.com>
2018-12-10 00:09:07 +0300

    with new features

--- VII/911b.py
+++ VII/911b.py
@@ -1,22 +1,33 @@
 import MyMath as mm
 from math import exp, sin, pi
 import numpy as np
-h = pi/50
-x = np.array([])
-y = np.array([])
-flag = 1
-delta = 1.0 #где мы сейчас
-I = 0
+h = pi/10
+
+flag_eps  =  1
+eps_appropriate = 10**(-3)
+
 def func(x):
     return exp(-x**2)*sin(x)
-while flag:
-    x = np.append(x, delta)
-    y = np.append(y, func(delta))
-
-    delta += h
-    I = mm.int_trapeze(x, y)
-    if ((exp(-delta)) < (10**(-5))*I):
-        flag = 0
+while flag_eps:
+    delta = 1.0 #где мы сейчас
+    I = 0
+    flag = 1
+    x = np.array([])
+    y = np.array([])
+    while flag:
+        x = np.append(x, delta)
+        y = np.append(y, func(delta))
+        delta += h
+        I = mm.int_trapeze(x, y)
+        if ((exp(-delta)) < (10**(-5))*I):
+            flag = 0
+    eps = ((delta-1)*(h**2)/6)/I
+    print(eps, I)
+    if eps < eps_appropriate:
+        flag_eps = 0
+        print("OK")
+    else:
+        h = h/2
+        print("We have problem with accuracy, current step is:", h)
 
 print(delta, I, exp(-delta))
-#print(I)
