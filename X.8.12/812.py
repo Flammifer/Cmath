21288be47e38bb3ed99e10a4fe2e9b5657b769c7
Flammifer <37635688+Flammifer@users.noreply.github.com>
2019-04-02 13:06:20 +0300

    Add files via upload

--- X.8.12/812.py
+++ X.8.12/812.py
@@ -1,6 +1,6 @@
 from math import exp
 import numpy as np
-J = ((998,  1998) ,(-999,-1999))
+J = np.array([(998,  1998) ,(-999,-1999)])
 h = [10**(-4), 10**(-3), 0.0025, 5*10**(-3), 0.01]
 def solution(t):
     return (-3*(exp(-1000*t)) + 4*(exp(-t)), 3*(exp(-1000*t)) -2*(exp(-t)) )
@@ -17,7 +17,19 @@ def Adams(h, ini_vec,  t_ini = 0.0, t_fin = 10.0):
 def check(vec, t):
     return (abs(vec[0]-solution(t)[0]) + abs(vec[1]-solution(t)[1]))/(abs(solution(t)[0]) + abs(solution(t)[1]))
 
-check(Adams(h[2], (1,1)), 10)
+
+def CROS(h, ini_vec, t_ini = 0.0, t_fin = 10.0):
+        t = t_ini
+        sol = np.array([ini_vec[0], ini_vec[1]])
+        E = np.array([[1,0], [0,1]])
+        while t < t_fin:
+            t  += h
+            tmp =np.linalg.solve(np.add(E, -h*complex(1/2, 1/2 )*J) , np.dot(J,sol) )
+            sol = np.add(sol, h*(tmp.real))
+        return sol
+
+
 
 for i in range(5):
-    print(check(Adams(h[i], (1,1)), 10))
+    print("Adams", check(Adams(h[i], (1,1)), 10))
+    print("CROS", check(CROS(h[i], (1,1)),10))
